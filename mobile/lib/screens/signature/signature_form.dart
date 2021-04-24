e989f68798c4c4554e26d8aefb09855fa92e41c4
AlexandreBessa <alexandrebessaluis@hotmail.com>
2021-04-24 10:12:00 -0300

    Fixing Form

--- mobile/lib/screens/signature/signature_form.dart
+++ mobile/lib/screens/signature/signature_form.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/models/signature.dart';
+import 'package:mobile/widgets/form_text.dart';
 
-class SignatureForm extends StatelessWidget {
+const _appBarTitle = 'Criar Assinatura';
+
+const _labelTitle = 'Título';
+const _labelValue = 'Valor';
+const _labelButton = 'Cadastrar';
+
+class SignatureForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SignatureFormState();
+  }
+}
+
+class SignatureFormState extends State<SignatureForm> {
+  final TextEditingController _controllerTitle = TextEditingController();
+  final TextEditingController _controllerValue = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Criar Assinatura'),
+        title: Text(_appBarTitle),
       ),
-      body: Column(children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            style: TextStyle(fontSize: 24.0),
-            decoration: InputDecoration(
-              labelText: 'Título',
-            ),
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          FormText(
+            controller: _controllerTitle,
+            label: _labelTitle,
           ),
-          // ElevatedButton(
-          //   child: Text('Cadastrar'),
-          //   onPressed: () {
-          //     debugPrint('cliclou no botão');
-          //   }
-          // ),
-        )
-      ]),
+          FormText(
+            controller: _controllerValue,
+            label: _labelValue,
+          ),
+          ElevatedButton(
+            child: Text(_labelButton),
+            onPressed: () => _formBuild(context),
+          ),
+        ]),
+      ),
     );
+  }
+
+  void _formBuild(BuildContext context) {
+    final String title = _controllerTitle.text;
+    final double value = double.tryParse(_controllerValue.text);
+    if (title != null && value != null) {
+      final formBuilded = Signature(title, value);
+      Navigator.pop(context, formBuilded);
+    }
   }
 }
--- mobile/lib/screens/signature/signature_list.dart
+++ mobile/lib/screens/signature/signature_list.dart
@@ -2,29 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:mobile/models/signature.dart';
 import 'package:mobile/screens/signature/signature_form.dart';
 
-class SignatureList extends StatelessWidget {
+const _appBarTitle = 'Assinaturas';
+
+class SignatureList extends StatefulWidget {
+  final List<Signature> _signatures = [];
+
+  @override
+  State<StatefulWidget> createState() {
+    return SignatureListState();
+  }
+}
+
+class SignatureListState extends State<SignatureList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Assinaturas'),
+        title: Text(_appBarTitle),
       ),
-      body: Column(
-        children: <Widget>[
-          SignatureItem(Signature('Amazon Prime', 9.90)),
-          SignatureItem(Signature('Spotify', 8.50)),
-          SignatureItem(Signature('Office 365', 23.99)),
-        ],
+      body: ListView.builder(
+        itemCount: widget._signatures.length,
+        itemBuilder: (context, index) {
+          final signature = widget._signatures[index];
+          return SignatureItem(signature);
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) {
             return SignatureForm();
-          }));
+          })).then(
+            (signatureReceived) => _reload(signatureReceived),
+          );
         },
       ),
     );
+  }
+
+  void _reload(Signature signatureReceived) {
+    if (signatureReceived != null) {
+      setState(() {
+        widget._signatures.add(signatureReceived);
+      });
+    }
   }
 }
 
